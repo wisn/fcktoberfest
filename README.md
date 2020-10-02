f20fcf7ae6613c4b50741191eeae8680cea5e8a6
Wisnu Adi Nurcahyo <wisn.adn@gmail.com>
2020-10-02 20:07:13 +0700

    Improved Docs are no more

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 Hacktoberfest spam PR slayer for your open source project.
 Free swag hunter can just **fck off**!
 
+## TODO
+
+- [x] Close PR with `improve(d) doc(s)` title
+- [ ] Close PR with a _very_ small diff
+- [ ] Close PR from recently created GitHub account (is it possible?)
+
 ## License
 
 Licensed under the [MIT License](LICENSE).
--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
 module.exports = app => {
-  // Your code here
-  app.log.info('Yay, the app was loaded!')
+  app.on('pull_request.opened', async context => {
+    let qualityPR = false;
+    const prTitle = context.payload.pull_request.title;
+    const qualityPRTitles = [/(improve)+d?\ (doc)+s?/i];
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+    qualityPRTitles.forEach(qualityPRTitle => {
+      if (!qualityPR) {
+        qualityPR = qualityPRTitle.test(prTitle);
+      }
+    });
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+    if (qualityPR) {
+      const invalidLabel = context.issue({ name: 'invalid' });
+      if (!context.github.issues.getLabel(invalidLabel)) {
+        context.github.issues.createLabel(invalidLabel);
+      }
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+      const spamLabel = context.issue({ name: 'spam' });
+      if (!context.github.issues.getLabel(spamLabel)) {
+        context.github.issues.createLabel(spamLabel);
+      }
+
+      const prComment = context.issue({
+        body: [
+          "Thank you for your contribution!",
+          "\n\nHowever, your pull request is identified as a spam.",
+          "That's why you got `invalid` & `spam` labels",
+          "and your pull request closed immediately.",
+          "\n\nIf you are doing this just for some free Hacktoberfest swags",
+          "then good luck and please don't come back!",
+          "\n\nIn the other hands, if you are doing quality contribution then",
+          "please mention the maintainers right away.",
+          "\n\n\nCheers!",
+        ].join(" "),
+      });
+      context.github.issues.createComment(prComment);
+
+      const assignLabels = context.issue({ labels: ['invalid', 'spam'] });
+      context.github.issues.addLabels(assignLabels);
+
+      const closePR = context.pullRequest({ state: 'closed' });
+      return context.github.pulls.update(closePR);
+    }
+  });
+};
